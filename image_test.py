e0cfdf588934bba560f21a0ae7277f4aac99f0af
Ahn Jiwan <synoti21@naver.com>
2023-10-15 00:51:46 +0900

    :white_check_mark: feat: print an inferenced value for comparing an actual value (#7)

--- image_test.py
+++ image_test.py
@@ -6,19 +6,19 @@ from utils.nutrition_runner import nutrition_run
 def colored_text(text: str, color_code: str) -> str:
     return f"\033[{color_code}m{text}\033[0m"
 
-
 class TestOCRAccuracy(unittest.TestCase):
     EXPECTED_VALUES = {
         'test5.jpg': {'내용량': 490, '칼로리':365, '탄수화물': 67, '지방': 7, '단백질': 8},
         'test6.jpg': {'내용량': 110, '칼로리': 446, '탄수화물': 99, '지방': 3, '단백질': 7},
         'test7.jpg': {'내용량': 120, '칼로리': 505, '탄수화물': 84, '지방': 15, '단백질': 9},
+
     }
 
-    def print_colored_result(self, key: str, is_matched: bool):
-        if is_matched:
-            print(colored_text(f"{key}: P", "92"), end=", ")  # 92: Bright Green
-        else:
-            print(colored_text(f"{key}: F", "91"), end=", ")  # 91: Bright Red
+    def print_results(self, label: str, values: dict, color_code: str):
+        print(colored_text(label, color_code), end=" ")
+        for key, val in values.items():
+            print(f"{key}: {val}", end=", ")
+        print()
 
     def test_ocr_for_images(self):
         total_matched = 0
@@ -31,34 +31,34 @@ class TestOCRAccuracy(unittest.TestCase):
 
             if not isinstance(ocr_result, dict):
                 print(colored_text(f"Failed to recognize text for {image_name}", "91"))
-                for key in expected_value:
-                    self.print_colored_result(key, False)
-                print("\n정확도: 0%")
-                print('-' * 40)
-                total_elements += 5
                 continue
 
+            self.print_results("기대", expected_value, "96")  # 96: Bright Cyan for expected
+
+            actual_values_colored = {}
             num_matched = 0
             for key, expected_val in expected_value.items():
-                is_matched = ocr_result.get(key) == expected_val
-                if is_matched:
+                actual_val = ocr_result.get(key)
+                if actual_val == expected_val:
+                    actual_values_colored[key] = colored_text(str(actual_val), "92")  # 92: Bright Green
                     num_matched += 1
-                self.print_colored_result(key, is_matched)
+                else:
+                    actual_values_colored[key] = colored_text(str(actual_val), "91")  # 91: Bright Red
+
+            self.print_results("실제", actual_values_colored, "97")  # 97: Default for actual
 
             total_matched += num_matched
             total_elements += len(expected_value)
 
             accuracy = (num_matched / len(expected_value)) * 100
-            print(f"\n정확도: {accuracy:.2f}%")
+            print(f"정확도: {accuracy:.2f}%")
             print('-' * 40)
 
         overall_accuracy = (total_matched / total_elements) * 100
         print(f"일치도: {total_matched}/{total_elements}, 정확도: {overall_accuracy:.2f}%")
 
-
 if __name__ == "__main__":
     unittest.main()
-
 
 
 
