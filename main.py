4f43a4bde21c1340783aa9bf8fbb9cef2821aa0e
Ahn Jiwan <synoti21@naver.com>
2023-11-29 20:43:22 +0900

    :sparkles: feat: 방법 1: 쓰레딩 사용 - 26초
    총 시간은 29초에서 26초로 줄었지만, 사용자 1이 24초, 사용자 2가 26초로 됨으로써 더 비효율적. GIL 때문에 동시 작업이 불가 (#28)

--- main.py
+++ main.py
@@ -1,23 +1,20 @@
-import os
-import traceback
-
 import cv2
-import boto3
-import configparser
-
 import numpy as np
 from starlette.responses import JSONResponse
 from utils.image_preprocess import PreProcessor
 from fastapi import FastAPI, HTTPException, File, UploadFile
-from botocore.exceptions import ClientError
 from pydantic import BaseModel
 import logging
 import warnings
 
 from utils.nutrition_runner import nutrition_run
 from utils.pororo_ocr import PororoOcr
+from concurrent.futures import ProcessPoolExecutor
+import asyncio
+
 
 warnings.filterwarnings('ignore')
+executor = ProcessPoolExecutor(max_workers=10)
 
 
 
@@ -32,6 +29,11 @@ preprocessor = PreProcessor()
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
+
+async def run_in_executor(func, *args):
+    loop = asyncio.get_event_loop()
+    result = await loop.run_in_executor(executor, func, *args)
+    return result
 
 @app.get("/")
 def health_check():
@@ -51,7 +53,8 @@ async def read_item(file: UploadFile = File(...)):
 
     image = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
 
-    result = nutrition_run(image)
+    result = await run_in_executor(nutrition_run, image)
+
     if not result:
         raise HTTPException(status_code=422, detail='Text Recognition Fail')
     else:
